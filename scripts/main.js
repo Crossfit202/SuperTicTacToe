a1529d6eb0e0723c366f388fe500e89a33ab3ad2
Joncross202 <cro21035@byui.edu>
2024-06-12 21:56:01 -0400

    Changes class and ID structure
    Co-authored-by: Jonathan9719 <Jonathan9719@users.noreply.github.com>

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,32 @@ var lastMove = 'Blank';
 
 function Move() {
     var div = this;
-    if (lastMove === 'Blank') {
-        div.textContent = 'X';
-        div.classList.add('X')
-        lastMove = 'X';
-    } else if (lastMove === 'X') {
-        div.textContent = 'O';
-        div.classList.add('O')
-        lastMove = 'O';
-    } else {
-        div.textContent = 'X';
-        div.classList.add('X')
-        lastMove = 'X';
+    if (div.textContent == '') {
+        if (lastMove === 'Blank') {
+            div.textContent = 'X';
+            div.classList.add('X')
+            lastMove = 'X';
+        } else if (lastMove === 'X') {
+            div.textContent = 'O';
+            div.classList.add('O')
+            lastMove = 'O';
+        } else {
+            div.textContent = 'X';
+            div.classList.add('X')
+            lastMove = 'X';
+        }
     }
+}
+
+function nextMoveLocation() {
+    var board = document.getElementsByClassName('board3')
+    board.classList.add('highlight');
+
+
 }
 
 var singleBoards = document.getElementsByClassName('single_board');
 
 for (var i = 0; i < singleBoards.length; i++) {
-    singleBoards[i].addEventListener('click', Move);
+    singleBoards[i].addEventListener('click', Move, nextMoveLocation);
 }
